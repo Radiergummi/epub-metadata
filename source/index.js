3f1df2b39917d3ad912a720524c666b2465e04e1
Mo <m@9dev.de>
2017-04-25 09:43:14 +0200

    Updated formatting, added documentation and moved to ES6

--- source/index.js
+++ source/index.js
@@ -1,113 +1,159 @@
-var path = require('path'),
+'use strict';
 
-	fsp = require('fs-promise'),
-	JsZip = require('jszip'),
-	xmlMapping = require('xml-mapping')
+/*
+ global module,
+ require
+ */
 
+const path       = require('path'),
+      fsp        = require('fs-promise'),
+      JsZip      = require('jszip'),
+      xmlMapping = require('xml-mapping');
 
+/**
+ *
+ * @param {string} key
+ * @param {object} value
+ * @returns {*}
+ */
 function cleanUpMetadata (key, value) {
-	// Remove namespace part from keys
-	if (typeof key === 'string' && /\$/.test(key)) {
-		var newKey = key.split('$')[1]
-		this[newKey] = value
-		delete this[key]
-		key = newKey
-	}
 
-	// Make text property the actual value
-	if (value.hasOwnProperty('$text') &&
-		Object.keys(value).length === 1) {
-		this[key] = value.$text
-	}
+  // Remove namespace part from keys
+  if (typeof key === 'string' && /\$/.test(key)) {
+    let newKey     = key.split('$')[ 1 ];
+    this[ newKey ] = value;
+    delete this[ key ];
+    key = newKey;
+  }
 
-	return value
+  // Make text property the actual value
+  if (value.hasOwnProperty('$text') && Object.keys(value).length === 1) {
+    this[ key ] = value.$text;
+  }
+
+  return value;
 }
 
+/**
+ *
+ * @param {object} json
+ * @param {object} json.package
+ * @param {object} json.package.metadata
+ * @param metadata
+ */
 function loadDcMetadata (json, metadata) {
-	for (var key in json.package.metadata) {
-		if (key.search('dc') === 0) {
-			!function () {
-				var newKey = key.replace(/^dc\$/, '')
-				metadata[newKey] = json.package.metadata[key]
-			}()
-		}
-	}
+  for (let key in json.package.metadata) {
+    if (!json.package.metadata.hasOwnProperty(key)) {
+      continue;
+    }
+
+    if (key.search('dc') === 0) {
+      !function() {
+        let newKey         = key.replace(/^dc\$/, '');
+        metadata[ newKey ] = json.package.metadata[ key ];
+      }()
+    }
+  }
 }
 
+/**
+ *
+ * @param {object} json
+ * @param {object} json.package
+ * @param {object} json.package.metadata
+ * @param {Array}  json.package.metadata.meta
+ */
 function rewriteMetaElements (json) {
-	json.package.metadata.meta.forEach(function (meta) {
-		json.package.metadata['dc$' + meta.name] = meta.content
-	})
-	delete json.package.metadata.meta
+  json.package.metadata.meta.forEach(meta => {
+    json.package.metadata[ 'dc$' + meta.name ] = meta.content;
+  });
+
+  delete json.package.metadata.meta;
 }
 
+/**
+ *
+ * @param   {string} contentPath
+ * @param   {string} coverId
+ * @param   {object} json
+ * @param   {object} json.package
+ * @param   {object} json.package.manifest
+ * @returns {string}
+ */
 function getCoverImagePath (contentPath, coverId, json) {
-	var href = ''
-	json.package.manifest.item.some(function (item) {
-		if (item.id === coverId) {
-			href = path.join(contentPath, '..', item.href)
-			return true
-		}
-	})
-	return href
+  let href = '';
+
+  json.package.manifest.item.some(item => {
+    if (item.id === coverId) {
+      href = path.join(contentPath, '..', item.href);
+
+      return true;
+    }
+  });
+
+  return href;
 }
 
-module.exports = function (epubPath) {
+/**
+ *
+ * @param   {string} epubPath
+ * @returns {Promise.<object>}
+ */
+module.exports = function(epubPath) {
+  return fsp
+    .readFile(epubPath)
+    .then(fileContent => {
 
-	return fsp
-		.readFile(epubPath)
-		.then(function (fileContent) {
+      let epub         = new JsZip(fileContent),
+          metadata     = {},
+          contentPaths = [
+            'content.opf',
+            'OEBPS/content.opf'
+          ],
+          contentPath,
+          json,
+          xml;
 
-			var epub = new JsZip(fileContent),
-				metadata = {},
-				contentPaths = [
-					'content.opf',
-					'OEBPS/content.opf'
-				],
-				contentPath,
-				json,
-				xml
+      contentPaths.some(contentFilePath => {
+        xml         = epub.file(contentFilePath);
+        contentPath = contentFilePath;
 
-			contentPaths.some(function (contentFilePath) {
-				xml = epub.file(contentFilePath)
-				contentPath = contentFilePath
-				return Boolean(xml)
-			})
+        return Boolean(xml);
+      });
 
-			if (!xml)
-				throw new Error(
-					'The software was not able to locate ' +
-					'a content.opf file in ' + epubPath
-				)
+      if (!xml) {
+        throw new Error(`The software was not able to locate a content.opf file in ${epubPath}`);
+      }
 
-			json = xmlMapping.load(xml.asText(), {longTag: true})
+      json = xmlMapping.load(xml.asText(), {
+        longTag: true
+      });
 
-			rewriteMetaElements(json)
+      rewriteMetaElements(json);
 
-			loadDcMetadata(json, metadata)
+      loadDcMetadata(json, metadata);
 
-			// This is a little hack to iterate recursively over an object
-			JSON.stringify(metadata, cleanUpMetadata)
+      // This is a little hack to iterate recursively over an object
+      JSON.stringify(metadata, cleanUpMetadata);
 
-			// Use keys for identifiers
-			if (!Array.isArray(metadata.identifier))
-				metadata.identifier = [metadata.identifier]
+      // Use keys for identifiers
+      if (!Array.isArray(metadata.identifier)) {
+        metadata.identifier = [ metadata.identifier ];
+      }
 
-			metadata.identifier.forEach(function (identifier) {
-				metadata[identifier.scheme.toLowerCase()] = identifier.text
-			})
+      metadata.identifier.forEach(
+        identifier => metadata[ identifier.scheme.toLowerCase() ] = identifier.text
+      );
 
-			delete metadata.identifier
+      delete metadata.identifier;
 
-			metadata.coverPath = getCoverImagePath(
-				contentPath,
-				metadata.cover,
-				json
-			)
+      metadata.coverPath = getCoverImagePath(
+        contentPath,
+        metadata.cover,
+        json
+      );
 
-			return metadata
-		})
-		.catch(function (error) {
-			console.error(error.stack)
-		})
-}
+      return metadata;
+    })
+    .catch(error => console.error(error.stack));
+};
